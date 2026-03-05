069aaa9775824f718f357df2cbcfa2aeadaa2f7c
otilevad <arthurdavelvb@gmail.com>
2026-03-05 01:56:47 -0300

    MÚLTIPLAS IMAGENS, APENAS ME DIGA QUAIS PASTAS SERÃO ABERTAS HOJE...

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
 from tkinter import *
 from tkinter import filedialog
-import os
+from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
-img = Image.open(filedialog.askopenfilename())
+# img = Image.open(filedialog.askopenfilename())
 
+origem = Path(filedialog.askdirectory())
 destino = filedialog.askdirectory()
-image_name = img.filename.split('/')[-1].split('.')
-nome = f'{image_name[0]}-MagoStamper3000.{image_name[1]}'
 
-img = ImageOps.exif_transpose(img)
+images = []
+for ext in ['*.png', '*.jpg', '*.jpeg']:
+    images.extend(origem.glob(ext))
 
-width, height = img.size
+for image in images:
+    img = Image.open(image)
+    image_name = img.filename.split('\\')[-1].split('.')
 
-bigger_side = max(width, height)
+    exif_data = img.getexif()
+    date_time = exif_data.get(306, "Unknown")
 
-font_size = (bigger_side/35.45)
-font_stroke = (font_size/21.66)
-margin = (bigger_side/13.55)
-
-exif_data = img.getexif()
- 
-date_time = exif_data.get(306, "Unknown")
-print(f"Date and Time: {date_time}")
-
-font= ImageFont.truetype('fonts/Comic Sans MS.ttf', font_size)
-draw = ImageDraw.Draw(img)
-
-text = "magodavos esteve aqui..."
-
-try:
-    date_time_array = date_time.split()
-    date_array = date_time_array[0].split(':')
-    time_array = date_time_array[1].split(':')
-    print(date_array[0])
-    text = (f"{date_array[2]}/{date_array[1]}/{date_array[0]} {time_array[0]}:{time_array[1]}")
-except:
     text = "magodavos esteve aqui..."
 
-bbox = draw.textbbox((0, 0), text=text, font=font)
-textwidth = bbox[2] - bbox[0]
-textheight = bbox[3] - bbox[1]
+    try:
+        date_time_array = date_time.split()
+        date_array = date_time_array[0].split(':')
+        time_array = date_time_array[1].split(':')
+        text = (f"{date_array[2]}/{date_array[1]}/{date_array[0]} {time_array[0]}:{time_array[1]}")
+    except:
+        text = "magodavos esteve aqui..."
 
-x = width - textwidth - margin
-y = height - textheight - margin
+    print(f"Estampando {text} em: {img.filename}")
 
-draw.text((x, y), text, font = font, fill=(253, 162, 0), stroke_width=font_stroke, stroke_fill=(0, 0, 0))
-#img.show()
+    nome = f'{image_name[0]}-MagoStamper3000.{image_name[1]}'
 
-path_inteiro = f'{destino}/{nome}'
+    img = ImageOps.exif_transpose(img)
 
-img.save(path_inteiro)
+    width, height = img.size
+
+    big_side = max(width, height)
+
+    font_size = (big_side/35.45)
+    font_stroke = (font_size/21.66)
+    margin = (big_side/13.55)
+
+    font= ImageFont.truetype('fonts/Comic Sans MS.ttf', font_size)
+    draw = ImageDraw.Draw(img)
+
+    bbox = draw.textbbox((0, 0), text=text, font=font)
+    textwidth = bbox[2] - bbox[0]
+    textheight = bbox[3] - bbox[1]
+
+    x = width - textwidth - margin
+    y = height - textheight - margin
+
+    draw.text((x, y), text, font = font, fill=(253, 162, 0), stroke_width=font_stroke, stroke_fill=(0, 0, 0))
+    #img.show()
+
+    path_inteiro = f'{destino}/{nome}'
+
+    img.save(path_inteiro)

67e3c7502c30dce11f484942925551dd30d4f948
otilevad <arthurdavelvb@gmail.com>
2026-03-05 00:55:54 -0300

    primeirão

---
+++ main.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+img = Image.open(filedialog.askopenfilename())
+width, height = img.size
+
+exif_data = img.getexif()
+ 
+date_time = exif_data.get(306, "Unknown")
+print(f"Date and Time: {date_time}")
+
+date_time_array = date_time.split()
+date_array = date_time_array[0].split(':')
+time_array = date_time_array[1].split(':')
+print(date_array[0])
+
+draw = ImageDraw.Draw(img)
+text = (f"{date_array[2]}/{date_array[1]}/{date_array[0]} {time_array[0]}:{time_array[1]}")
+
+font= ImageFont.truetype('fonts/Comic Sans MS.ttf', 130)
+
+bbox = draw.textbbox((0, 0), text=text, font=font)
+textwidth = bbox[2] - bbox[0]
+textheight = bbox[3] - bbox[1]
+
+margin = 340
+x = width - textwidth - margin
+y = height - textheight - margin
+
+draw.text((x, y), text, font = font, fill=(253, 162, 0), stroke_width=6, stroke_fill=(0, 0, 0))
+#img.show()
+
+destino = filedialog.askdirectory()
+image_name = img.filename.split('/')[-1].split('.')
+nome = f'{image_name[0]}-MagoStamper3000.{image_name[1]}'
+
+path_inteiro = f'{destino}/{nome}'
+
+img.save(path_inteiro)
+
+img.show()
